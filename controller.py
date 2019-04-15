be022f404f448d8952374ae26e7c0c7db7776b83
unknown <ra.stanik@hotmail.com>
2019-04-14 20:14:08 -0700

    Added some comments to controller.py and added __pycache__/ to .gitignore

--- controller.py
+++ controller.py
@@ -1,4 +1,11 @@
+import salary
 # where the program starts
 # determines which city and which job to scrape data from
+
+#if user enters "Software Engineer" and "Bay Area"
+userRequest = Salary("Bay Area", "Software Engineer")
+
+#if user enters "Software Engineer" and "Seattle"
+#userRequest = salary("Seattle", "Software Engineer")
 
 print("hello world!")
