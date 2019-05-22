d7b1c05f63c642ea5a9d844be59b0f041c155744
Andrea Hernandez <andreaeu02@gmail.com>
2019-05-21 17:50:52 -0700

    KB in python

---
+++ KnowledgeBase.py
@@ -0,0 +1,56 @@
+import math
+
+class Person:
+    def __init__(self, city, jobTitle, yearOfExperience, salary):
+        self.city = city
+        self.jobTitle = jobTitle
+        self.yearOfExpereince = yearOfExperience
+        self.salary = salary
+
+
+class KB:
+
+    def predictSalary(person):
+        percentage = percentage(person.yearOfExperience)
+        salaryByExperience = 0
+        salary = person.salary
+
+        if percentage >= 0:
+            salaryByExperience = (salary * (1 + (percentage / 100)))
+            return salaryByExperience
+        else:
+            salaryByExperience = (salary * (1 - (percentage / 100)))
+            return salaryByExperience
+
+        return salaryByExperience
+
+
+    def percentageByExperience(self, yearOfExperince):
+        if yearOfExperience >= 0 and yearOfExperience <= 5:
+            return -10
+        elif yearOfExperience > 5 and yearOfExperience < 15:
+            return 5
+        elif yearOfExperience >= 15 and yearOfExperience < 25:
+            return 19
+        elif yearOfExperience >= 25:
+            return 28
+
+
+
+if __name__ == '__main__':
+
+    kb1 = KB()
+    user = Person("San Jose", "Software Engineer", 12, 120000)
+    predictedSalary = 0
+    predictedSalary = kb1.predictSalary(kb1, user)
+
+    print( "Initial Salary: " )
+    print(user.salary)
+    print("      The AI predicted salary for a ")
+    print(user.jobTitle)
+    print(" in ")
+    print(user.city)
+    print(" with ")
+    print(user.yearOfExperience)
+    print(" years of experience is: ")
+    print(predictedSalary)
